dcac06359e1b0e9309109fd6fcb60abcfc7a8d4d
unknown <sfqtuhin04@gmail.com>
2022-10-22 22:21:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * Title: Uptime Monitoring Application
+ * Description: A Restful API to monitor up or downtime of user defined links
+ * Author: Md Shafiqul Islam
+ * Date: 22/10/22
+ */
+
+// Dependencies
+const http = require('http');
